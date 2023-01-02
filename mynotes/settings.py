3a4ce2ca5e443c640840bccdcc58abad113824d8
aditya yadav <91489989+adiityayadav@users.noreply.github.com>
2023-01-02 13:38:58 +0530

    Update settings.py

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -85,14 +85,12 @@ WSGI_APPLICATION = 'mynotes.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
  
- DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
